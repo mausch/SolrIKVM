3c3990021d7891b80ada18f9560f6e93ae354e02
mausch <mauricioscheffer@gmail.com>
2011-02-06 22:44:41 -0300

    port and solr home to config

--- SolrIKVM/Jetty.cs
+++ SolrIKVM/Jetty.cs
@@ -1,13 +1,26 @@
-﻿using org.apache.solr.servlet;
+﻿using System.Configuration;
+using System.IO;
+using org.apache.solr.servlet;
 using org.mortbay.jetty;
 using org.mortbay.jetty.servlet;
 
 namespace SolrIKVM {
     public static class Jetty {
+        public static int Port {
+            get {
+                try {
+                    return int.Parse(ConfigurationManager.AppSettings["port"]);
+                } catch {
+                    return 8983;
+                }
+            }
+        }
+
         public static int Main(string[] args) {
-            java.lang.System.setProperty("solr.solr.home", @"..\..");
-            java.lang.System.setProperty("solr.data.dir", @"..\..\data");
-            var server = new Server(8983);
+            var home = ConfigurationManager.AppSettings["solr.home"];
+            java.lang.System.setProperty("solr.solr.home", home);
+            java.lang.System.setProperty("solr.data.dir", Path.Combine(home, "data"));
+            var server = new Server(Port);
             var servletContext = new Context(server, "/solr");
             var servlet = new SolrServlet();
             servletContext.addServlet(new ServletHolder(servlet), "/*");
