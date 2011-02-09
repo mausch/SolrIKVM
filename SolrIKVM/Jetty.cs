793da91abd6356cbe82123606c08970182654375
mausch <mauricioscheffer@gmail.com>
2011-02-09 19:20:19 -0300

    split port config to Setup

--- SolrIKVM/Jetty.cs
+++ SolrIKVM/Jetty.cs
@@ -4,23 +4,10 @@ using org.apache.solr.client.solrj.embedded;
 
 namespace SolrIKVM {
     public static class Jetty {
-        public static int Port {
-            get {
-                var p = ConfigurationManager.AppSettings["port"];
-                if (string.IsNullOrEmpty(p))
-                    return 8983;
-                try {
-                    return int.Parse(p);
-                } catch (Exception e) {
-                    throw new Exception(string.Format("Invalid port '{0}'", p), e);
-                }
-            }
-        }
-
         public static int Main(string[] args) {
             var home = ConfigurationManager.AppSettings["solr.home"];
             Setup.SetHome(home);
-            var jetty = new JettySolrRunner("/solr", Port);
+            var jetty = new JettySolrRunner("/solr", Setup.Port());
             jetty.start();
             return 0;
         }
--- SolrIKVM/Setup.cs
+++ SolrIKVM/Setup.cs
@@ -1,10 +1,25 @@
-﻿using System.IO;
+﻿using System;
+using System.Configuration;
+using System.IO;
 
 namespace SolrIKVM {
     public static class Setup {
         public static void SetHome(string path) {
             java.lang.System.setProperty("solr.solr.home", path);
             java.lang.System.setProperty("solr.data.dir", Path.Combine(path, "data"));
+        }
+
+        public static Func<int> Port = GetPortFromConfig;
+
+        public static int GetPortFromConfig() {
+            var p = ConfigurationManager.AppSettings["port"];
+            if (string.IsNullOrEmpty(p))
+                return 8983;
+            try {
+                return int.Parse(p);
+            } catch (Exception e) {
+                throw new Exception(string.Format("Invalid port '{0}'", p), e);
+            }
         }
     }
 }
